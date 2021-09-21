f2eccd7169e6177a609f73179f7e4317d80116b2
Dima Faller <ded@turboded.ru>
2021-09-22 00:15:40 +0300

    1

--- user/extras/header-pre.tmpl.php
+++ user/extras/header-pre.tmpl.php
@@ -1,2 +1,46 @@
+<div class="common">
 
-<div style="color: rgb(187, 187, 187); background-color: rgb(40, 44, 52); font-family: Consolas, &quot;Courier New&quot;, monospace; line-height: 19px; white-space: pre;"><span style="color: #abb2bf;">src:</span>&nbsp;<span style="color: #98c379;">url</span>(<span style="color: #e5c07b;">"/fonts/IBMPlexMono-Bold.ttf"</span>)&nbsp;<span style="color: #98c379;">format</span>(<span style="color: #e5c07b;">"truetype"</span>)<span style="color: #abb2bf;">,</span></div>
+
+
+<div class="flag">
+
+<div class="header-box">
+  <div class="header-photo-link">
+    <a href="/" style="color: black;"><img class="header-photo" src="https://sugrarin.ru/user/userpic@2x.jpg?1619350841" alt="Фото автора"></a>
+  </div>
+  <div>
+    <h1 class="author-name">Тимур Суграрин</h1>
+    <nav class="menu">
+      <ul class="menu-links">
+        <li>
+          <a href="https://sugrarin.ru/" style="color: black;">Лента</a>
+        </li>
+        <li>
+          <a href="https://sugrarin.ru/all/">Все&nbsp;заметки</a>
+        </li>
+        <li>
+          <a href="https://sugrarin.ru/tags/projects/">Проекты</a>
+        </li>
+        <li>
+          <a href="https://sugrarin.ru/all/lists/">Подборки</a>
+        </li>
+        <li>
+          <a href="https://sugrarin.ru/tags/advices/">Советы</a>
+        </li>
+        <li>
+          <a href="https://sugrarin.ru/tags/links/">Ссылки</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>
+
+<script>
+const href = document.location.href
+document.querySelectorAll('.header-box a').forEach(element => {
+        if (element.href === href) {
+            element.style.color = 'black'
+        }
+    })
+</script>
+</div>
