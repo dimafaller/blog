5d3d413c96502c8f922995f8235dba17117cf8e6
Dima Faller <ded@turboded.ru>
2021-09-25 03:37:47 +0300

    hkgb;lk'

--- user/extras/header-pre.tmpl.php
+++ user/extras/header-pre.tmpl.php
@@ -5,7 +5,7 @@
         <li>
           <a href="https://turboded.ru/blog">Лента</a>
         </li>
-          <a href="https://turboded.ru/blog/tags/chto-posmotret/">Что&nbsp;Посмотреть</a>
+          <a href="https://turboded.ru/blog/tags/chto-posmotret/">Посмотрел</a>
         </li>
         <li>
           <a href="https://turboded.ru/blog/tags/nahodki/">Нашел</a>
